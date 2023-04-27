cbcf5b1278de0e06860b6592a76ad970b623a942
Dmitry Dziraev <dziraevdmitry@gmail.com>
2023-04-27 18:06:18 +0300

    add loader to Button component

--- src/common/fields/buttons/Button/Button.tsx
+++ src/common/fields/buttons/Button/Button.tsx
@@ -1,14 +1,14 @@
 import styles from './Button.module.css';
 
-interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   isLoading?: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({ children, isLoading = false }) => {
+export const Button: React.FC<ButtonProps> = ({ children, isLoading = true }) => {
   return (
     <button className={styles.button}>
       {!isLoading && children}
-      {!isLoading && children}
+      {isLoading && <div className={styles.loading} />}
     </button>
   );
 };
